0b0e74e61b9b61a37cf8230050f7a4ca527835e0
ianagbip1oti <ianagbip1oti@gmail.com>
2019-04-18 09:27:28 +0000

    Refinement of ruby example

--- ruby/lib/pingbot.rb
+++ ruby/lib/pingbot.rb
@@ -1,26 +1,12 @@
-require_relative 'smalld-examples-ruby_jars'
-java_import com.github.princesslana.smalld.SmallD
-
-require 'json'
-require 'hash_dot'
 
 
-SmallD.run(ENV['SMALLD_TOKEN'].to_s) do |smalld|
-  reply = lambda do |opts|
-    channel = opts[:to].channel_id
-    msg = { 'content' => opts[:with] }.to_json
+require_relative 'bot'
 
-    smalld.post "/channels/#{channel}/messages", msg
-  end
-
-  on_message_create = lambda do |msg|
-    reply.call to: msg, with: 'pong' if msg.content == '++ping'
-  end
-
-  smalld.onGatewayPayload do |payload|
-    j = JSON.parse(payload).to_dot
-
-    on_message_create.call(j.d) if j.t == 'MESSAGE_CREATE'
+ping_bot = bot do |s|
+  s.on_message_create do |msg|
+    s.post "/channels/#{msg.channel_id}/messages", content: 'pong' if msg.content == '++ping'
   end
 end
+
+ping_bot.run token: ENV['SMALLD_TOKEN']
 

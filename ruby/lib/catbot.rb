6853c8ea1eca888cc25ef3dd5dbd44a676852bb3
ianagbip1oti <ianagbip1oti@gmail.com>
2019-04-19 06:53:16 +0000

    Add ruby catbot

---
+++ ruby/lib/catbot.rb
@@ -0,0 +1,24 @@
+
+require_relative 'bot'
+
+require 'httparty'
+require 'json'
+
+def send_cat(smalld, channel_id)
+  response = HTTParty.get  'http://aws.random.cat/meow'
+
+  cat_url = JSON.parse(response.body)['file']
+
+  smalld.post(
+    "/channels/#{channel_id}/messages",
+    '',
+    RAttachment.new('cat.jpg', 'image/jpeg', cat_url))
+end
+
+cat_bot = Bot.new do |smalld|
+  smalld.on_message_create do |msg|
+    send_cat smalld, msg.channel_id if msg.content == '++cat'
+  end
+end
+
+cat_bot.run token: ENV['SMALLD_TOKEN'] if __FILE__ == $0
